0542d45d96c19e4dd213ef9732bcade946bad0b4
Sheena <11192150+sheenacodes@users.noreply.github.com>
2023-10-09 14:30:33 +0300

    fix issues raised by ruff

--- tests/test_api_cesva.py
+++ tests/test_api_cesva.py
@@ -1,6 +1,5 @@
 import logging
 import os
-from datetime import datetime, timedelta, timezone
 import json
 import httpx
 
@@ -11,39 +10,27 @@ API_TOKEN = os.getenv("API_TOKEN", "abc123")
 
 
 # query params
-PARAMS = {
-    "x-api-key": API_TOKEN
-}
+PARAMS = {"x-api-key": API_TOKEN}
 
 # Body
-PAYLOAD = {"sensors":[
-	{
-		"sensor":"TA120-T246187-N",
-		"observations":[
-			{"value":"61.2", "timestamp":"24/02/2022T17:45:15UTC"}
-		]
-	},{
-		"sensor":"TA120-T246187-O",
-		"observations":[
-			{"value":"false", "timestamp":"24/02/2022T17:45:15UTC"}
-		]
-	},{
-		"sensor":"TA120-T246187-U",
-		"observations":[
-			{"value":"false", "timestamp":"24/02/2022T17:45:15UTC"}
-		]
-	},{
-		"sensor":"TA120-T246187-M",
-		"observations":[
-			{"value":"77", "timestamp":"24/02/2022T17:45:15UTC"}
-		]
-	},{
-		"sensor":"TA120-T246187-S",
-		"observations":[
-			{"value":"060.6,0,0;060.8,0,0;060.4,0,0;059.9,0,0;059.9,0,0;060.6,0,0;060.7,0,0;060.4,0,0;059.9,0,0;059.9,0,0;060.2,0,0;060.4,0,0;060.8,0,0;059.9,0,0;059.7,0,0;060.6,0,0;061.1,0,0;061.1,0,0;060.6,0,0;061.3,0,0;061.4,0,0;060.6,0,0;060.7,0,0;060.6,0,0;061.4,0,0;061.3,0,0;061.0,0,0;060.3,0,0;059.6,0,0;059.6,0,0;060.4,0,0;062.0,0,0;062.6,0,0;061.7,0,0;061.8,0,0;062.5,0,0;061.8,0,0;062.1,0,0;061.6,0,0;061.1,0,0;063.0,0,0;061.2,0,0;061.1,0,0;060.6,0,0;060.1,0,0;061.8,0,0;062.1,0,0;062.4,0,0;061.5,0,0;061.9,0,0;062.5,0,0;061.9,0,0;061.1,0,0;061.8,0,0;062.0,0,0;062.2,0,0;062.2,0,0;062.9,0,0;062.2,0,0;061.3,0,0", "timestamp":"24/02/2022T17:45:15UTC"}
-		]
-	}
-]}
+PAYLOAD = {
+    "sensors": [
+        {"sensor": "TA120-T246187-N", "observations": [{"value": "61.2", "timestamp": "24/02/2022T17:45:15UTC"}]},
+        {"sensor": "TA120-T246187-O", "observations": [{"value": "false", "timestamp": "24/02/2022T17:45:15UTC"}]},
+        {"sensor": "TA120-T246187-U", "observations": [{"value": "false", "timestamp": "24/02/2022T17:45:15UTC"}]},
+        {"sensor": "TA120-T246187-M", "observations": [{"value": "77", "timestamp": "24/02/2022T17:45:15UTC"}]},
+        {
+            "sensor": "TA120-T246187-S",
+            "observations": [
+                {
+                    "value": "060.6,0,0;060.8,0,0;060.4,0,0;059.9,0,0;059.9,0,0;060.6,0,0; \
+                    060.7,0,0;060.4,0,0;059.9,0,0;059.9,0,0;060.2,0,0;060.4,0,0;",
+                    "timestamp": "24/02/2022T17:45:15UTC",
+                }
+            ],
+        },
+    ]
+}
 
 
 def test_service_up():
@@ -57,7 +44,9 @@ def test_cesva_endpoint_up():
     url = f"{API_BASE_URL}/api/v1/cesva"
     resp = httpx.put(url)
     assert resp.status_code == 401, "error: /api/v1/cesva accessible without token"
-    assert resp.text.startswith("Missing or invalid authentication token"), "error: /api/v1/cesva accessible without token"
+    assert resp.text.startswith(
+        "Missing or invalid authentication token"
+    ), "error: /api/v1/cesva accessible without token"
 
 
 def test_cesva_endppoint_authenticated_access():
@@ -75,8 +64,8 @@ def test_cesva_endppoint_authenticated_access():
 
 
 def main():
-    #test_service_up()
-    #test_cesva_endpoint_up()
+    # test_service_up()
+    # test_cesva_endpoint_up()
     test_cesva_endppoint_authenticated_access()
 
 
